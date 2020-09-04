4db270de5531d6258c924b93387aff36b4e3ac2e
整笑了 给👴🏻 <cccsssyyy@seiwarairyous-MacBook-Pro.local>
2020-09-04 11:40:20 -0400

    my first commit

--- src/madeira/cs/Lab1.java
+++ src/madeira/cs/Lab1.java
@@ -20,10 +20,26 @@ public class Lab1 {
        */
 
         // Part I code below
-
+       String s1;
+       s1 = "String1";
+       System.out.println(s1);
+       String s2;
+       s2 = "String2";
+       System.out.println(s2);
+       String s3;
+       s3 = "String3";
+       System.out.println(s3);
         // Part II code below
-
+       s1="NewString1";
+       System.out.println(s1);
+       s2="NewString2";
+       System.out.println(s2);
+       s3="NewString3";
+       System.out.println(s3);
         // Part III code below
+       System.out.println(s1.length());
+       System.out.println(s1.replace("New", "Old"));
+       System.out.println(s1.toLowerCase());
 
     }
 }
