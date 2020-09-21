e6da9ab102ec1d13d141985867b0aeb8535d6f12
整笑了 给👴🏻 <cccsssyyy@seiwarairyous-MacBook-Pro.local>
2020-09-21 10:58:41 -0400

    my first commit

---
+++ src/madeira/cs/SuperLab.java
@@ -0,0 +1,22 @@
+package madeira.cs;
+
+public class SuperLab {
+	/*
+		Gebrauchsanweisung
+		-------
+		Write a program that does the following
+		I. Creates a new class named Artist.
+			a. Artist should have 3 member variables
+				(a)i. name
+				(a)ii. album
+				(a)iii. twitterHandle
+				(a)iv. rating
+		II. Creates 6 methods to set & get each of these values + 1 main method
+		III. Uses the main method to open the text file artistList.txt
+		IV. Constructs an Artist for each of the entries in the list & adds them to an Array
+			a. This will require: reading the size of the list and constructing the list to match
+		V. Print each artist entry
+			a. Make sure that you've constructed a rating member too! This is not in the file!
+	*/
+
+}
